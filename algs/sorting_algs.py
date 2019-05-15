3cf837529e288c157b28ebbb368b1730a0ed8e2d
Narange <nmv29@cornell.edu>
2019-05-14 22:29:14 -0400

    insertion sort

--- algs/sorting_algs.py
+++ algs/sorting_algs.py
@@ -2,5 +2,16 @@
 
 
 # Insertion sort
-def insertion_sort(input_list):
-    pass
+def insertion_sort(A):
+    # for each elem, starting at 2nd elem (1st is already sorted)
+    for i in range(1, len(A)):
+        # for elem to the left of this one
+        for j in range(i - 1, -1, -1):
+
+            # elem to left of current index > elem at current index
+            if A[j] > A[j + 1]:
+                # swap the elems
+                A[j], A[j + 1] = A[j + 1], A[j]
+            else:
+                break
+    return A
