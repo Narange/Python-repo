79638cebeeedcea1d423e38740ac3b1cdf8acb09
Narange <nmv29@cornell.edu>
2019-05-19 13:16:49 -0400

    added quicksort, which is beautiful

--- algs/sorting_algs.py
+++ algs/sorting_algs.py
@@ -1,7 +1,8 @@
 """Sorting methods"""
+from random import randint
 
 
-# Insertion sort
+# Insertion Sort
 def insertion_sort(A):
     # for each elem, starting at 2nd elem (1st is already sorted)
     for i in range(1, len(A)):
@@ -15,3 +16,23 @@ def insertion_sort(A):
             else:
                 break
     return A
+
+
+# Quick Sort
+def quicksort(A):
+    if len(A) <= 1:
+        return A
+
+    lower, equal, higher = [], [], []
+    pivot = A[randint(0, len(A) - 1)]  # select a random pivot value
+
+    # put values into the 3 lists based on comparison to pivot
+    for i in A:
+        if i < pivot:
+            lower.append(i)
+        elif i == pivot:
+            equal.append(i)
+        else:
+            higher.append(i)
+
+    return quicksort(lower) + equal + quicksort(higher)
--- algs/sorting_main.py
+++ algs/sorting_main.py
@@ -5,7 +5,7 @@ import sorting_algs
 
 
 # Return a list of size length - each elem is random int in range given
-# Ex) random_list(5, (1, 100)) -> A list of 5 random ints from 1 to 100
+# Ex: random_list(10, (1, 100)) -> A list of 10 random ints from 1 to 100
 def random_list(length, span):
     the_list = []
     for i in range(length):
@@ -15,6 +15,10 @@ def random_list(length, span):
 
 # Generate a random lists, sort them, and display the before/after
 
-a_list = random_list(5, (1, 100))
+a_list = random_list(10, (1, 100))
 print("Input list:\n" + str(a_list))
-print("Insertion sort:\n" + str(sorting_algs.insertion_sort(a_list)))
+print("Insertion sort:\n" + str(sorting_algs.insertion_sort(a_list)) + "\n")
+
+a_list = random_list(10, (1, 100))
+print("Input list:\n" + str(a_list))
+print("Quicksort:\n" + str(sorting_algs.quicksort(a_list)) + "\n")
